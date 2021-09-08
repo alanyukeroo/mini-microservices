e31ef9e7d725d85ef2b714bb9400c113daba1ba8
alanyukeroo@gmail.com <alanyukeroo@gmail.com>
2021-09-09 01:07:56 +0700

    update

---
+++ posts/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const bodyParser = require('body-parser')
+const {randomBytes} = require('crypto');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    // const roomTest = randomBytes(4).toString('hex')
+    // res.send(roomTest).json;
+    res.send(posts);
+});
+
+app.post('/posts', (req, res)=>{
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body;  
+    // OR
+    // const body = req.body;
+    // title = body.title = req.body.title;
+    posts[id] = {
+        id, title
+    };
+
+    /*
+    posts = [{id: randomByte, title: "title"},{id: randomByte, title: "title"},{id: randomByte, title: "title"},{id: randomByte, title: "title"},]
+    */
+
+    res.status(201).send(posts[id]);
+    // res.send(id)    
+
+});
+
+
+app.listen(4000, ()=> {
+    console.log('Listening on 4000')
+});
+
